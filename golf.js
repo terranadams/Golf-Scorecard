e3b16359a01db5c9b861d5daf55602386feab7ec
terranadams <terran.adams7225@stu.mtec.edu>
2020-10-22 19:01:17 -0600

    trying to push player names to players array

--- golf.js
+++ golf.js
@@ -1,9 +1,34 @@
 let course
-let playerCount = document.getElementById('playerCount').value
+let playerCount
 
-let nameInputHTML = ''
+let players = []
+
+
+let golfCourse = fetch('https://golf-courses-api.herokuapp.com/courses/')
+    .then(res => res.json())
+
+
+
+let nameInputHTML = `<h4>Gimme names</h4>`
+
 
 function getNames() {
-    console.log(playerCount)
-}
+    course = document.getElementById('course').value
+    playerCount = document.getElementById('playerCount').value
 
+
+    for (let i = 0 ; i < playerCount; i++) {
+        nameInputHTML += `
+            <input id="playerNumber${i}" type="text" placeholder="Enter name "><br>
+        `;
+        
+    }
+    nameInputHTML += '<br><button onclick="startGame()">Start</button>'
+    document.getElementById('names').innerHTML = nameInputHTML
+} 
+function startGame() {
+    for (let i = 0; i > playerCount; i++) {
+        players.push(document.getElementById(`playerNumber${i}`).value) // This isnt pushing anything to the array
+    }
+    console.log(players)
+}
