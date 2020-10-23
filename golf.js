a17dfa15f9f1f84bc263984955d0c6baa650cccf
terranadams <terran.adams7225@stu.mtec.edu>
2020-10-23 09:33:44 -0600

    got api object

--- golf.js
+++ golf.js
@@ -1,31 +1,29 @@
 let course
 let playerCount
-
 let players = []
 
-
-let golfCourse = fetch('https://golf-courses-api.herokuapp.com/courses/')
-    .then(res => res.json())
-
-
+let golfCourse
+let myReq = new XMLHttpRequest();
+myReq.open('GET','https://golf-courses-api.herokuapp.com/courses/', true )
+myReq.onload = function() {
+    golfCourse = JSON.parse(myReq.responseText)
+    console.log(golfCourse)
+}
+myReq.send();
 
 let nameInputHTML = `<h4>Gimme names</h4>`
-
-
 function getNames() {
     course = document.getElementById('course').value
     playerCount = document.getElementById('playerCount').value
-
-
     for (let i = 0 ; i < playerCount; i++) {
         nameInputHTML += `
             <input id="playerNumber${i}" type="text" placeholder="Enter name "><br>
         `;
-        
     }
     nameInputHTML += '<br><button onclick="startGame()">Start</button>'
     document.getElementById('names').innerHTML = nameInputHTML
 } 
+
 function startGame() {
     for (let i = 0; i > playerCount; i++) {
         players.push(document.getElementById(`playerNumber${i}`).value) // This isnt pushing anything to the array
