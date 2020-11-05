db2a13d22921c9409fb58b6478cab333d44e03b1
terranadams <terran.adams7225@stu.mtec.edu>
2020-11-05 09:26:30 -0700

    changes

--- javascript.js
+++ javascript.js
@@ -38,22 +38,40 @@ function getNames() {
 function startGame() {
     for (let i = 0; i < playerCount; i++) {
         players.push(document.getElementById(`playerNumber${i}`).value);
-        let newPlayerHTML = `<div class="row">
+
+        let newPlayerHTML1 = `<div class="row">
         <div class="col-1 leftColumn player${i}">${players[i]}</div>
-        <div class="col-1 text-center"></div>
-        <div class="col-1 text-center"></div>
-        <div class="col-1 text-center"></div>
-        <div class="col-1 text-center"></div>
-        <div class="col-1 text-center"></div>
-        <div class="col-1 text-center"></div>
-        <div class="col-1 text-center"></div>
-        <div class="col-1 text-center"></div>
-        <div class="col-1 text-center"></div>
-        <div class="col-1 text-center"></div>
+        <div class="col-1 text-center" id="player${i}hole1"></div>
+        <div class="col-1 text-center" id="player${i}hole2"></div>
+        <div class="col-1 text-center" id="player${i}hole3"></div>
+        <div class="col-1 text-center" id="player${i}hole4"></div>
+        <div class="col-1 text-center" id="player${i}hole5"></div>
+        <div class="col-1 text-center" id="player${i}hole6"></div>
+        <div class="col-1 text-center" id="player${i}hole7"></div>
+        <div class="col-1 text-center" id="player${i}hole8"></div>
+        <div class="col-1 text-center" id="player${i}hole9"></div>
+        <div class="col-1 text-center" id="player${i}out"></div>
+        <div class="col-1 text-center" id="player${i}total"></div>
       </div>`
-        document.getElementById('cardStuff1').innerHTML += newPlayerHTML
-        document.getElementById('cardStuff2').innerHTML += newPlayerHTML
+        document.getElementById('cardStuff1').innerHTML += newPlayerHTML1;
+
+        let newPlayerHTML2 = `<div class="row">
+        <div class="col-1 leftColumn player${i}">${players[i]}</div>
+        <div class="col-1 text-center" id="player${i}hole10"></div>
+        <div class="col-1 text-center" id="player${i}hole11"></div>
+        <div class="col-1 text-center" id="player${i}hole12"></div>
+        <div class="col-1 text-center" id="player${i}hole13"></div>
+        <div class="col-1 text-center" id="player${i}hole14"></div>
+        <div class="col-1 text-center" id="player${i}hole15"></div>
+        <div class="col-1 text-center" id="player${i}hole16"></div>
+        <div class="col-1 text-center" id="player${i}hole17"></div>
+        <div class="col-1 text-center" id="player${i}hole18"></div>
+        <div class="col-1 text-center" id="player${i}in"></div>
+        <div class="col-1 text-center" id="player${i}total"></div>
+      </div>`
+        document.getElementById('cardStuff2').innerHTML += newPlayerHTML2
     }
     
     document.getElementById('courseName').innerHTML = course
+    document.getElementById('names').remove();
 }
